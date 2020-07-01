925933be2faab50550c35434d63a6eb0e27e5f14
Kristina Dj <djkristinaxx@gmail.com>
2020-07-01 08:39:55 +0200

    Updatedt text and train files

--- split_dataset.py
+++ split_dataset.py
@@ -3,26 +3,26 @@ import os
 import sys
 
 
-def split_dataset(dataset_dir):
+def split_dataset(dataset_curr_dir, dataset_target_dir):
     test_file = open("test.txt", 'w')
     train_file = open("train.txt", 'w')
 
-    path, dirs, files = next(os.walk(dataset_dir))
+    path, dirs, files = next(os.walk(dataset_curr_dir))
 
     data_size = len(files)
     test_dataset_size = int(0.2 * data_size)
     test_dataset_file_indices = random.sample(range(data_size), k=test_dataset_size)
     idx = 0
 
-    for f in os.listdir(dataset_dir):
+    for f in os.listdir(dataset_curr_dir):
         if f.split(".")[4] == "jpg":
             idx += 1
 
             if idx in test_dataset_file_indices:
-                test_file.write(dataset_dir + '\\' + f + '\n')
+                test_file.write(dataset_target_dir + '/' + f + '\n')
             else:
-                train_file.write(dataset_dir + '\\' + f + '\n')
+                train_file.write(dataset_target_dir + '/' + f + '\n')
 
 
 if __name__ == "__main__":
-    split_dataset(sys.argv[1])
+    split_dataset(sys.argv[1], sys.argv[2])
