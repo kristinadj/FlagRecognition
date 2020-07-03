23974c6753bff1aa9bef77c0f6d32d7b0d74b649
Kristina Dj <djkristinaxx@gmail.com>
2020-07-03 10:33:53 +0200

    Changed split script, added cfg for testing

--- split_dataset.py
+++ split_dataset.py
@@ -5,20 +5,26 @@ import sys
 
 def split_dataset(dataset_curr_dir, dataset_target_dir):
     test_file = open("test.txt", 'w')
+    valid_file = open("valid.txt", 'w')
     train_file = open("train.txt", 'w')
 
     path, dirs, files = next(os.walk(dataset_curr_dir))
 
     data_size = len(files)
-    test_dataset_size = int(0.2 * data_size)
-    test_dataset_file_indices = random.sample(range(data_size), k=test_dataset_size)
+    test_and_valid_dataset_size = int(0.3 * data_size)
+    test_and_valid_dataset_file_indices = random.sample(range(data_size), k=test_and_valid_dataset_size)
+    
+    valid_dataset_size = int(0.5 * test_and_valid_dataset_size)
+    valid_dataset_file_indices = random.sample(test_and_valid_dataset_file_indices, k=valid_dataset_size)
     idx = 0
 
     for f in os.listdir(dataset_curr_dir):
         if f.split(".")[1] == "jpg":
             idx += 1
 
-            if idx in test_dataset_file_indices:
+            if idx in valid_dataset_file_indices:
+                valid_file.write(dataset_target_dir + '/' + f + '\n')
+            elif idx in test_and_valid_dataset_file_indices:
                 test_file.write(dataset_target_dir + '/' + f + '\n')
             else:
                 train_file.write(dataset_target_dir + '/' + f + '\n')
